8ce9d36a19048061c0dc76dae523be5f650741c9
WhyFenceCode <whyfencecode@gmail.com>
2025-02-12 19:05:08 -0800

    added backend conections that allow for simple data inclusion

--- app/scan/[slug]/page.tsx
+++ app/scan/[slug]/page.tsx
@@ -14,17 +14,16 @@ import {
 } from "@/components/ui/breadcrumb"
 
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 import {
-  landing_start_prompts,
-  landing_end_prompts,
+  articles,
 } from "@/data/homepage/text"
+
 import { ChevronsRight } from 'lucide-react';
 
 export default async function Page({
@@ -33,72 +32,112 @@ export default async function Page({
     params: Promise<{ slug: string }>
 }) {
   const slug = (await params).slug
+  const slugInt: number = Number((await params).slug);
+  const articleUrl = articles[slugInt]?.url
+  const questionOne = articles[slugInt]?.questionOne
+  const questionTwo = articles[slugInt]?.questionTwo
+  const questionThree = articles[slugInt]?.questionThree
 
-  const carouselStartItems = landing_start_prompts.map(landing_start_prompts => 
-    <CarouselItem>
-      <div className="p-4">
-        <h1 className="text-xl sm:text-4xl text-foreground mb-2">{landing_start_prompts.title}</h1>
-        <p className="text-l sm:text-2xl text-muted-foreground">{landing_start_prompts.text}</p>
+  if (articleUrl == undefined) {
+    return (
+      <div className="min-h-screen flex flex-col">
+        <div className="absolute bottom-0 left-0 -z-10 h-full w-full bg-background bg-[radial-gradient(circle_farthest-corner_at_50%_110%,hsl(var(--primary-glow))_0%,hsl(var(--background))_50%,hsl(var(--background))_100%)] bg-cover	"></div>
+        <header className="p-8">
+          <div className="w-auto h-4 flex items-center">
+          <Breadcrumb>
+              <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbEllipsis />
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{slug}</BreadcrumbPage>
+                  </BreadcrumbItem>
+              </BreadcrumbList>
+          </Breadcrumb>
+          </div>
+        </header>
+
+        <main className="flex-grow flex items-center justify-center">
+            <div className="w-full sm:max-w-[70%] max-h-[90%] max-w-[90%] flex items-center justify-center flex-col sm:flex-row">
+              <h1 className='text-3xl sm:text-4xl font-extrabold mb-4 max-w-6xl text-muted-foreground leading-loose'>Scan Code Not Found</h1>  
+            </div>
+        </main>
+
+        <footer className="p-6 flex">
+          <ThemeIcon />
+          <div className='grow'></div>
+          <Button size="lg" asChild>
+            <Link href="/"> <ChevronsRight /> To Article </Link>
+          </Button>
+        </footer>
       </div>
-    </CarouselItem>
-  )
+    );
+  } else {
+    return (
+      <div className="min-h-screen flex flex-col">
+        <div className="absolute bottom-0 left-0 -z-10 h-full w-full bg-background bg-[radial-gradient(circle_farthest-corner_at_50%_110%,hsl(var(--primary-glow))_0%,hsl(var(--background))_50%,hsl(var(--background))_100%)] bg-cover	"></div>
+        <header className="p-8">
+          <div className="w-auto h-4 flex items-center">
+          <Breadcrumb>
+              <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbEllipsis />
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{slug}</BreadcrumbPage>
+                  </BreadcrumbItem>
+              </BreadcrumbList>
+          </Breadcrumb>
+          </div>
+        </header>
 
-  const carouselEndItems = landing_end_prompts.map(landing_end_prompts => 
-    <CarouselItem>
-      <div className="p-4">
-        <h1 className="text-xl sm:text-4xl text-foreground mb-2">{landing_end_prompts.title}</h1>
-        <p className="text-l sm:text-2xl text-muted-foreground">{landing_end_prompts.text}</p>
+        <main className="flex-grow flex items-center justify-center">
+            <div className="w-full sm:max-w-[70%] max-h-[90%] max-w-[90%] flex items-center justify-center flex-col sm:flex-row">
+              <Card className="sm:w-[30%] m-2">
+                <CardHeader>
+                  <CardTitle>Question One</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1>{questionOne}</h1>
+                </CardContent>
+              </Card>
+              <Card className="sm:w-[30%] m-2">
+                <CardHeader>
+                  <CardTitle>Question Two</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1>{questionTwo}</h1>
+                </CardContent>
+              </Card>
+              <Card className="sm:w-[30%] m-2">
+                <CardHeader>
+                  <CardTitle>Question Three</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1>{questionThree}</h1>
+                </CardContent>
+              </Card>
+            </div>
+        </main>
+
+        <footer className="p-6 flex">
+          <ThemeIcon />
+          <div className='grow'></div>
+          <Button size="lg" asChild>
+            <Link href={articleUrl}> <ChevronsRight /> Home </Link>
+          </Button>
+        </footer>
       </div>
-    </CarouselItem>
-  )
-  
-  return (
-    <div className="min-h-screen flex flex-col">
-      <div className="absolute bottom-0 left-0 -z-10 h-full w-full bg-background bg-[radial-gradient(circle_farthest-corner_at_50%_110%,hsl(var(--primary-glow))_0%,hsl(var(--background))_50%,hsl(var(--background))_100%)] bg-cover	"></div>
-      <header className="p-8">
-        <div className="w-auto h-4 flex items-center">
-        <Breadcrumb>
-            <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbEllipsis />
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{slug}</BreadcrumbPage>
-                </BreadcrumbItem>
-            </BreadcrumbList>
-        </Breadcrumb>
-        </div>
-      </header>
-
-      <main className="flex-grow flex items-center justify-center">
-          <Carousel className="w-full max-w-[70%] max-h-[90%]">
-            <CarouselContent>
-              {carouselStartItems}
-              {/* The Questions Will Be Added Here */}
-              {carouselEndItems}
-              <CarouselItem>
-                <div className="p-4 flex flex-col items-center justify-center text-center">
-                  <h1 className="text-5xl sm:text-6xl font-extrabold mb-4 max-w-6xl text-foreground leading-loose">Your Article Awaits You</h1>
-                  <p className="text-xl sm:text-3xl mb-8 max-w-3xl text-muted-foreground">Press on the button bellow to take you to the article that you are supposed to read</p>
-                  <Button size="lg" asChild>
-                    <Link href="#"> <ChevronsRight /> To Article </Link>
-                  </Button>
-                </div>
-              </CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-      </main>
-
-      <footer className="p-6">
-        <ThemeIcon />
-      </footer>
-    </div>
-  );
+    )
+  }
 }
---
+++ data/homepage/text.ts
@@ -0,0 +1,14 @@
+export const homepage_app_blocks = {
+    app: "Name Goes Here",
+    landingText: "Main Text Here",
+    landingSubText: "Second Text Here",
+}
+
+export const articles: Record<number, { url: string; questionOne: string; questionTwo: string; questionThree: string; }> = {
+    6742: {
+        url: "https://google.com",
+        questionOne: "1",
+        questionTwo: "2",
+        questionThree: "3",
+    }
+}
