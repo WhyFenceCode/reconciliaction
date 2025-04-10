4e24f501f5a326aa7f5100a4ee90c4b8955b2c92
WhyFenceCode <whyfencecode@gmail.com>
2025-04-09 20:02:39 -0700

    feat: added about

---
+++ app/about/page.tsx
@@ -0,0 +1,63 @@
+import {
+    homepage_app_blocks,
+} from "@/data/homepage/text"
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbEllipsis,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
+
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ slug: string }>
+}) {
+  const slug = (await params).slug
+
+    return (
+      <div className="min-h-screen flex flex-col">
+        <div className="absolute bottom-0 left-0 -z-10 h-full w-full bg-background bg-[radial-gradient(circle_farthest-corner_at_50%_110%,hsl(var(--primary-glow))_0%,hsl(var(--background))_50%,hsl(var(--background))_100%)] bg-cover	"></div>
+        <header className="p-8">
+          <div className="w-auto h-4 flex items-center">
+          <Breadcrumb>
+              <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbEllipsis />
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{slug}</BreadcrumbPage>
+                  </BreadcrumbItem>
+              </BreadcrumbList>
+          </Breadcrumb>
+          </div>
+        </header>
+
+        <main className="flex-grow flex items-center justify-center">
+            <div className="w-full sm:max-w-[70%] max-h-[90%] max-w-[90%] flex items-center justify-center flex-col sm:flex-row">
+              <Card className="sm:w-[32%] w-[90%] m-2">
+                <CardContent>
+                  <h1>
+                    Welcome to {homepage_app_blocks.landingText}! We are a group of students focused on tackling Indigenous issues regarding education by fostersing genuine empathy. We have built a webiste that poses guiding questions surrounding topics and curated articles as a way to build engagment. Enjoy!
+                  </h1>
+                </CardContent>
+              </Card>
+            </div>
+        </main>
+      </div>
+    )
+}
--- data/homepage/text.ts
+++ data/homepage/text.ts
@@ -5,34 +5,52 @@ export const homepage_app_blocks = {
 }
 
 export const articles: Record<number, { url: string; questionOne: string; questionTwo: string; questionThree: string; }> = {
-    0o1: {
+    1001: {
         url: "https://www.statcan.gc.ca/o1/en/plus/7072-national-day-truth-and-reconciliation-time-remember-reflect-and-learn",
         questionOne: "What forms of racism continue towards indigenous people in this day and age?",
         questionTwo: "In what ways are indigenous people under or over represented?",
         questionThree: "What trends are noticable in indigenous cultures?",
     },
-    0o2: {
+    1002: {
         url: "https://globalnews.ca/news/10785147/bc-conservatives-undrip-repeal-indigenous-rights-law-john-rustad",
         questionOne: "What us UNDRIP and why is it an important part of the rights of indigenous people?",
         questionTwo: "Why do the indigenous people want or not want the government to do?",
         questionThree: "Using what process are the indigenous people reclaiming their rights?",
     },
-    0o3: {
+    1003: {
         url: "https://www.cbc.ca/news/indigenous/b-c-land-act-dripa-1.7112974",
         questionOne: "What powers do the proposed amendment give indigenous peoples?",
         questionTwo: "Why does the conservative party call this \"fear mongering\"?",
         questionThree: "Why, does Terry Teegee argue that the proposed amendment is not \"fear mongering\"",
     },
-    0o4: {
+    1004: {
         url: "https://engage.gov.bc.ca/govtogetherbc/engagement/komoks-treaty/",
         questionOne: "Who are the K’ómoks First Nations and where are they located?",
         questionTwo: "How is a \"fair\" election being ensured?",
         questionThree: "What are \"modern treaties\"?",
     },
-    0o5: {
+    1005: {
         url: "https://hashilthsa.com/news/2025-01-27/usma-among-six-named-2024-bc-reconciliation-awards",
         questionOne: "What are the reconcilliation awards?",
         questionTwo: "How has reconciliation evolved over the years?",
         questionThree: "What is the impact of giving out an award like this?",
+    },
+    1006: {
+        url: "https://www.cbc.ca/news/politics/murray-sinclair-death-1.7372862",
+        questionOne: "What were Murray Sinclair's contributions and how did these impact Indigenous Communities?",
+        questionTwo: "What responses do you think communities had when Murray Sinclair became the first Indigenous judge in 1988?",
+        questionThree: "In what way has this news article broadened your perspective on the Indigenous and government relations?",
+    },
+    1007: {
+        url: "https://www.cbc.ca/news/canada/kitchener-waterloo/painting-with-clay-1.7455516",
+        questionOne: "How can the inequalities in the healthcare system be addressed and acted upon by the government?",
+        questionTwo: "How does racism and inequality affect still affect Indigenous peoples to this day?",
+        questionThree: "\"There is too much discussion [in the Federal Government] about what to do and few tangible results.\" Why do you think this is the case?",
+    },
+    1008: {
+        url: "https://www.cbc.ca/news/canada/kitchener-waterloo/painting-with-clay-1.7455516",
+        questionOne: "What is the significance of artwork in regards to Indigenous Communities?",
+        questionTwo: "Only around 3% of artists are Indigenous peoples. How can there be more representation and diversity in art?",
+        questionThree: "How have Indigenous traditions been passed down? (Use examples from the news article, as well as any of your own.)",
     },
 }
